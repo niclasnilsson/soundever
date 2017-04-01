4281c1fe84343ae428a6e05ad7ae8959fa414ff4
Niclas Nilsson <niclas@niclasnilsson.se>
2017-04-01 23:04:43 +0200

    Create and use multiple players.

--- src/cljs/soundever/app.cljs
+++ src/cljs/soundever/app.cljs
@@ -4,15 +4,33 @@
 (defn log [message]
   (.log js/console (pr-str message)))
 
+(def config
+  {
+    :traffic-load
+      { :type :volume
+        :player-id "player1"
+        :sample-rate 1000
+        :min 0
+        :max 200
+        :sound-source "https://arthead.io/sounds/CafeJazz.mp3"}
+    :number-of-users
+      { :type :volume
+        :player-id "player2"
+        :sample-rate 5000
+        :min 0
+        :max 1400
+        :sound-source "https://arthead.io/sounds/crow.mp3"}})
+
 (defonce data
   (r/atom
     {
-      ; :volume-stream (range 0 100 5)}))
-      :volume-stream [0 10 20 30 40 nil nil 70 80 90]}))
+      :traffic-load-stream    [0 10 20 30 40 50 60 70 80 90]
+      ; :traffic-load-stream    [0 100 200 300 400 1000 1500]
+      :number-of-users-stream [0 100 120 100 120 300 600 700]}))
 
-(defn set-volume [player-id volume]
- (let [player (.querySelector js/document player-id)]
-   (aset player "volume" (/ volume 100))))
+(defn set-volume [player-id percent]
+  (let [player (.querySelector js/document (str "#" player-id))]
+    (aset player "volume" percent)))
 
 (defn pop-value [coll-name]
   (let [coll (coll-name @data)
@@ -20,33 +38,77 @@
     (swap! data assoc coll-name (rest coll))
     value))
 
-(def volume-updater (js/setInterval
-                     (fn []
-                       (let [value (pop-value :volume-stream)]
-                         (if value
-                           (set-volume "#player1" value))))
-                     1000))
+(defn percent [min max value]
+  (Math/min (/ value (- max min)) 1.0))
 
-(defn slider [param min max value]
-  [:input {:type "range" :min min :max max :defaultValue value
-           :style {:width "100%"}
-           :on-change
-             (fn [e]
-               ; TODO: Är det här rätt tänkt?
-               (swap! data assoc param (int (.-target.value e)))
-               (set-volume "#player1" (int (.-target.value e))))}])
+(defn update-volume [stream-id player-id min max]
+  (fn [value]
+    (log (str "Setting volume to " value " on " player-id))
+    (let [value (pop-value stream-id)]
+      (if value
+        (set-volume player-id (percent min max value))))))
 
-(defn audio-player []
-  [:audio {:id :player1 :autoPlay true :controls true}
-    [:source {:src "https://arthead.io/sounds/CafeJazz.mp3"}]])
+; Timers
+; (def timer-update-volume
+;   (js/setInterval
+;     (update-volume :traffic-load-stream "#player1", 0 200)
+;     1000))
+
+(defn create-timer [f interval]
+  (log "Creating timer..")
+  (js/setInterval f interval))
+
+(defn kw [s]
+  (keyword (subs s 1)))
+
+(defn setup-stream [name config]
+  (let [stream-id (kw (str name "-stream"))
+        player-id (:player-id config)
+        min (:min config)
+        max (:max config)
+        interval (:sample-rate config)
+        update-fn (update-volume stream-id player-id min max)]
+    (create-timer update-fn interval)))
+
+
+(defn setup-system [config]
+  (doseq [[name stream-config] config]
+    (setup-stream name stream-config)))
+
+(defn audio-player [player-id src]
+  [:audio {:id player-id :autoPlay true :controls true :loop true}
+    [:source {:src src}]])
+
+(defn player [player-id source]
+  [:div
+   (audio-player player-id source)])
+
+(defn players [config]
+  (map
+    #(player (:player-id %1) (:sound-source %1))
+    (vals config)))
 
 (defn app-view []
   (let [volume (:volume @data)]
     [:div
-     [slider :volume 0 100 20]
      [:h3 (str "Volume is " volume)]
-     [audio-player]]))
+     (map
+       #(player (str "player" %) "https://arthead.io/sounds/CafeJazz.mp3")
+       (range 1 11))]))
 
 (defn init []
   (r/render-component [app-view]
                       (.getElementById js/document "container")))
+  ;(set-volume "player1" 0)
+  ;(set-volume "player2" 0))
+
+(setup-system config)
+
+; (defn slider [param min max value]
+;   [:input {:type "range" :min min :max max :defaultValue value
+;            :style {:width "100%"}
+;            :on-change
+;              (fn [e]
+;                ; TODO: Är det här rätt tänkt?
+;                (swap! data assoc param (int (.-target.value e)))
+;                (set-volume "#player1" (int (.-target.value e))))}])
